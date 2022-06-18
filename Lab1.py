2bd2458ca3f58f23aa829161bdc34d45866dde09
vashz <zatakiavashisth151@gmail.com>
2022-06-18 16:07:51 +0530

    Initial Commit

---
+++ Lab1.py
@@ -0,0 +1,114 @@
+# print("Hello World")
+# # comment
+
+# x=5
+# y='Vash'
+# print(x)
+# print(y)
+# x=y  #string
+# print(x)
+# x=y=z=5
+# print('Value of x is ',x)
+# print(type(x))
+#define a complex number find real and imaginary part and find absolute value
+# z=2j +5
+# print(type(z))
+# print("Real part= ", z.real)
+# print("Imaginary part= ", z.imag)
+# print("Absolute value= ",abs(z))
+
+# x=2e2
+# print(type(x))
+# print(x)
+
+# print(5**2)
+# print(5 // 2)
+# print(5 %2)
+
+
+# x=["Apple"]
+# y=["Apple"]
+# z=x
+# x.append(["hello"])
+# print(z)
+# print(x is y)
+# print(x==y)
+# help("keywords")
+
+# import random
+# print(random.randrange(1,10))
+# a="Hello world!"
+# print(a[1])
+# print(a[-1])
+# print(a[:-1])
+
+# #Q.2
+# a="Python is easy"
+# print(a[7:10])
+# print(a[12:])
+# print(a[3:6])
+# print(len(a))
+# print(a.strip())
+# print(a.lower())
+# print(a.split())
+
+# print("is" in a)
+
+# x=range(10)
+# for n in x:
+#     print(n)
+
+# lst=["hello",5,6,7]
+# mylis=["Hiii"]
+# print(lst)
+# for x in lst:
+#     print(x)
+# print("hello" in lst)
+# lst.append(9)
+# print(lst)
+# lst.insert(2,"hi")
+# print (lst)
+
+# lst.pop(1)
+# print(lst)
+
+# lst.clear()
+# print(lst)
+# mylist=lst.copy()
+# print(mylist)
+# mylis=lst
+# print(mylis)
+# print(lst+mylis)
+# lst.extend(mylis)
+# print(lst)
+
+#ques 1
+# print('''First line
+# Second line
+# Third line''')
+
+#ques 3
+# x="hello"
+# print(x)
+# x="help"
+# print(x)
+
+#ques 4
+# s="  hello "
+# print(s.strip())
+
+#ques5
+# import random
+# lst=[]
+# for n in range(10):
+#     lst.append(random.randrange(100,201))
+# print(min(lst))
+
+#ques 6
+# st="hello"
+# fst=st[:1]
+# lst=st[-1:]
+# print(lst+st[1:-1]+fst)
+
+#ques 8
+
